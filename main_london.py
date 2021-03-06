e888a0e4f8ef102ad491f165b9be3c1e7d8cc434
londonrosemary <79435573+londonrosemary@users.noreply.github.com>
2021-03-05 23:16:58 -0500

    London's first attempt
    Couldn't remember how to add returned input values back into the string.

---
+++ main_london.py
@@ -0,0 +1,12 @@
+madlib = "DING DING DING! The bell sound make all the children {VERB} to the {NOUN}. It's recess time! The time of day when {ADJECTIVE} kids can forget their {PLURALNOUN} and have some fun!"
+
+print("Verb:")
+VERB = input()
+print("Noun:")
+NOUN = input()
+print("Adjective:")
+ADJECTIVE = input()
+print("Plural Noun:")
+PLURALNOUN = input()
+
+print(mad_lib)
